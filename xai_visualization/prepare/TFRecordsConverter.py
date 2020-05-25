0edbc9004387939b766e4e66ae561a537442ac71
Maximilian Ammann <max@maxammann.org>
2020-05-25 22:40:31 +0200

    Experiment with tfrecords

--- xai_visualization/prepare/TFRecordsConverter.py
+++ xai_visualization/prepare/TFRecordsConverter.py
@@ -2,8 +2,11 @@ import tensorflow as tf
 import numpy as np
 from xai_visualization.util.load_data import load_folder
 
-def _float_feature(list_of_floats):  # float32
+def _float_feature(list_of_floats):
     return tf.train.Feature(float_list=tf.train.FloatList(value=list_of_floats))
+
+def _int_feature(list_of_ints):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=list_of_ints))
 
 class TFRecordsConverter:
     def __init__(self):
@@ -11,21 +14,22 @@ class TFRecordsConverter:
 
     def create_example(self, features, annotations):
         return tf.train.Example(features=tf.train.Features(feature={
-            'armscrossed': _float_feature(features[3]),
-            'label': _float_feature(annotations)}))
+            'armscrossed': _float_feature([features[3]]),
+            'label': _int_feature([annotations])}))
 
     def _write_tfrecord_file(self, raw_dir_path, output_dir_path):
         """Write TFRecord file."""
 
         sessions = load_folder(raw_dir_path)
-        with tf.io.TFRecordWriter("%s/sessions.tfrecord" % output_dir_path, options='ZLIB') as writer:
-            
+        with tf.io.TFRecordWriter("%s/train_sessions.tfrecord" % output_dir_path) as writer:
             for session in sessions:
                 expert_features, expert_annotations = session['expert']
                 novice_features, novice_annotations = session['novice']
 
-                writer.write(self.create_example(expert_features, expert_annotations).SerializeToString())
-                writer.write(self.create_example(novice_features, novice_annotations).SerializeToString())
+                for features, annotation in zip(expert_features, expert_annotations):
+                    writer.write(self.create_example(features, annotation).SerializeToString())
+                for features, annotation in zip(novice_features, novice_annotations):
+                    writer.write(self.create_example(features, annotation).SerializeToString())
 
 
     def convert(self, raw_dir_path, output_dir_path):
--- xai_visualization/train.py
+++ xai_visualization/train.py
@@ -3,18 +3,44 @@ from tensorflow.contrib.learn.python.learn.estimators._sklearn import train_test
 from tensorflow.python.keras.utils import to_categorical
 
 from xai_visualization.models.dense_model import create_model
-from xai_visualization.util.load_data import load_data
+import os
+
+feature_description = {
+    'armscrossed': tf.io.FixedLenFeature([], tf.float32, default_value=0),
+    'label': tf.io.FixedLenFeature([], tf.int64, default_value=0)
+}
+
+def _parse_function(example_proto):
+  # Parse the input `tf.Example` proto using the dictionary above.
+  example = tf.io.parse_single_example(example_proto, feature_description)
+
+  print(example)
+  return example['armscrossed'], example['label']
 
 
-def train():
-    features, annotations = load_data(r'../../korpus_no_video/027_2016-04-06_Nottingham/')
+def train(dataset_path):
+    tf.compat.v1.enable_eager_execution()
 
-    data_train, data_test, labels_train, labels_test = train_test_split(features, annotations, test_size=0.20,
-                                                                        random_state=42)
+    split = "train"
+    pattern = os.path.join(dataset_path, '{}*.tfrecord'.format(split))
+    files_ds = tf.data.Dataset.list_files(pattern)
+    ds = tf.data.TFRecordDataset(files_ds)
+
+    #ds = ds.batch(10)
+
+    parsed_dataset = ds.map(_parse_function)
+
+    for parsed_record in parsed_dataset.take(10):
+        print(repr(parsed_record))
+
+
+    # features, annotations = load_data(r'../../korpus_no_video/027_2016-04-06_Nottingham/')
+
+    # data_train, data_test, labels_train, labels_test = train_test_split(features, annotations, test_size=0.20,random_state=42)
 
     model = create_model()
-    model.fit(data_train, labels_train, batch_size=128, epochs=100, verbose=1, validation_data=(data_test, labels_test))
+    model.fit(parsed_dataset, epochs=100, steps_per_epoch=30, verbose=1)
 
-    data_test, labels_test = load_data(r'../../korpus_no_video/030_2016-04-06_Nottingham/')
-    score = model.evaluate(data_test, labels_test)
-    print(score)
+    # data_test, labels_test = load_data(r'../../korpus_no_video/030_2016-04-06_Nottingham/')
+    # score = model.evaluate(data_test, labels_test)
+    # print(score)
