a49b51a4083a23486c131046660bdcad5e27e11e
Maximilian Ammann <max@maxammann.org>
2020-05-26 17:58:49 +0200

    Use dropout layers

--- xai_visualization/models/dense_model.py
+++ xai_visualization/models/dense_model.py
@@ -1,10 +1,12 @@
 from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dense, Dropout
 
 def create_model():
     model = Sequential()
     model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
     model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.25))
     model.add(Dense(264, activation='relu'))
     model.add(Dense(4, activation='sigmoid'))
     return model
