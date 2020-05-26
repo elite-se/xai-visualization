ab9e7f3c06d437ba0ea0cf760f63ffe1f6437f7a
Maximilian Ammann <max@maxammann.org>
2020-05-26 16:16:18 +0200

    Refactor code

--- xai_visualization/train.py
+++ xai_visualization/train.py
@@ -13,10 +13,7 @@ EPOCHS = 10
 BATCH_SIZE = 256
 BUFFER_SIZE = 10000
 
-
-def train(dataset_path):
-    #tf.compat.v1.enable_eager_execution()
-
+def flatten_data(dataset_path):
     all_features = []
     all_annotations = []
 
@@ -27,28 +24,38 @@ def train(dataset_path):
                 all_features.append(feature)
                 all_annotations.append(label)
 
+    return all_features, all_annotations
+
+def prepare_dataset(dataset_path):
+    all_features, all_annotations = flatten_data(dataset_path)
 
     data_train, data_test, labels_train, labels_test = train_test_split(np.array(all_features), np.array(all_annotations), test_size=0.20, random_state=42)
 
-    print(np.array(all_features).shape)
+    train_data_single = tf.data.Dataset.from_tensor_slices(
+        (np.array(data_train), np.array(labels_train))
+    ).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
 
-    train_data_single = tf.data.Dataset.from_tensor_slices((np.array(data_train), np.array(labels_train)))
-    train_data_single = train_data_single.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+    val_data_single = tf.data.Dataset.from_tensor_slices(
+        (data_test, labels_test)
+    ).batch(BATCH_SIZE).repeat()
 
-    val_data_single = tf.data.Dataset.from_tensor_slices((data_test, labels_test))
-    val_data_single = val_data_single.batch(BATCH_SIZE).repeat()
+
+    steps_per_epoch = int(data_train.shape[0] / BUFFER_SIZE)
+
+    return train_data_single, val_data_single, steps_per_epoch
+
+def train(dataset_path):
+    #tf.compat.v1.enable_eager_execution()
+
+    train_data_single, val_data_single, steps_per_epoch = prepare_dataset(dataset_path)
 
     model = create_model()
 
     model.compile(optimizer='adam', loss=categorical_crossentropy, metrics=['accuracy'])
-    model.fit(train_data_single, epochs=EPOCHS,
-                                            steps_per_epoch=EVALUATION_INTERVAL,
-                                            validation_data=val_data_single,
-                                            validation_steps=50)
 
-
-       
-
-    # data_test, labels_test = load_data(r'../../korpus_no_video/030_2016-04-06_Nottingham/')
-    # score = model.evaluate(data_test, labels_test)
-    # print(score)
+    model.fit(train_data_single, 
+                    epochs=EPOCHS,
+                    steps_per_epoch=steps_per_epoch,
+                    validation_data=val_data_single,
+                    validation_steps=50
+    )
