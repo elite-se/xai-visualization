d9449a68aaedd4cd49d20f95399584ab280f3ff2
Maximilian Ammann <max@maxammann.org>
2020-05-26 17:11:37 +0200

    Fix numpy version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         'scipy==1.4.1',
         'matplotlib==3.2.1',
         'scikit-learn==0.23.1',
-        'numpy==1.18.4',
+        'numpy==1.16.4',
         'tensorflow==1.13.1',
         'lime==0.2.0.0',
         'hickle==3.4.6'
