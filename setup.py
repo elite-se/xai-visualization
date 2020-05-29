d2b76ea375cd78415238cefce0cce4f60e17d06e
Maximilian Ammann <max@maxammann.org>
2020-05-29 11:19:22 +0200

    Use required version of tensorflow for innvestigate

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'matplotlib==3.2.1',
         'scikit-learn==0.23.1',
         'numpy==1.16.4',
-        'tensorflow==2.2.0',
+        'tensorflow==2.1',
         'lime==0.2.0.0',
         'hickle==3.4.6',
         'innvestigate==2.0.0'
