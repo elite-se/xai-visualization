e7a5b820cc29b44dada1125ca129033b70c3d8c4
FabianWildgrube <fabian.wildgrube@gmx.de>
2020-06-08 14:39:57 +0200

    added chart-js dependency and first chart

--- frontend/src/ExplanationsContainer.tsx
+++ frontend/src/ExplanationsContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { HorizontalBar } from "react-chartjs-2";
 
 const Container = styled.div`
     flex-grow: 1;
@@ -11,12 +12,27 @@ const Container = styled.div`
 
     border: 1px solid gray;
     border-left: 0;
-`
+
+    box-sizing: border-box;
+    padding: 0 8px;
+`;
 
 function ExplanationsContainer() {
     return (
         <Container>
             <h2>EXPLANATIONS</h2>
+            <HorizontalBar
+                data={{
+                    labels: ["Face", "Valence", "Arms Crossed", "Hands"],
+                    datasets: [
+                        {
+                            label: "Testing Explanations",
+                            backgroundColor: "#00998C",
+                            data: [0.4, 0.2, 0.15, 0.1],
+                        },
+                    ],
+                }}
+            />
         </Container>
     );
 }
