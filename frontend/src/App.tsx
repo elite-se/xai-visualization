2ce102b33ebcc46674a7be56906639f66f3b707a
FabianWildgrube <fabian.wildgrube@gmx.de>
2020-06-08 15:14:17 +0200

    added some styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,30 @@
 import React from "react";
 import Participant from "./Participant";
 
-import {Icon, Navbar, NavbarGroup, NavbarHeading} from "@blueprintjs/core";
-import {IconNames} from "@blueprintjs/icons";
-import styled from 'styled-components'
+import { Icon, Navbar, NavbarGroup, NavbarHeading } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+import styled from "styled-components";
 
 const Container = styled.div`
     min-height: 100vh;
     width: 100%;
     box-sizing: border-box;
-`
+`;
 
 const Main = styled.main`
     padding: 40px;
-`
+`;
 
 const H3 = styled.div`
-        margin-left: 6px;
-`
-
+    margin-left: 6px;
+`;
 
 function App() {
     return (
         <Container>
             <Navbar>
                 <NavbarGroup>
-                    <Icon icon={IconNames.FULL_STACKED_CHART} iconSize={Icon.SIZE_LARGE}/>
+                    <Icon icon={IconNames.FULL_STACKED_CHART} iconSize={Icon.SIZE_LARGE} />
                     <NavbarHeading>
                         <H3>XAI-Visualisations</H3>
                     </NavbarHeading>
