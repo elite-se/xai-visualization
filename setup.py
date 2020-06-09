ee7184116523b1bae5d90c1c7a0228bc934e6ee0
Maximilian Ammann <max@maxammann.org>
2020-06-09 17:08:31 +0200

    Experiment with OpenCV

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import find_packages, setup
 setup(
     name='xai-visualization',
     packages=['xai_visualization', 'xai_visualization.models', 'xai_visualization.util', 'xai_visualization.prepare'],
-    scripts=['scripts/xai-train', 'scripts/xai-evaluation', 'scripts/xai-prepare', 'scripts/xai-plot', 'scripts/xai-explain'],
+    scripts=['scripts/xai-train', 'scripts/xai-evaluation', 'scripts/xai-prepare', 'scripts/xai-plot', 'scripts/xai-explain', 'scripts/xai-playback'],
     version='0.1.0',
     description='HCI solutions',
     author='maxammann',
     license='MIT',
     install_requires=[
+        'opencv-python',
+        'imutils==0.5.3',
         'scipy==1.4.1',
         'matplotlib==3.2.1',
         'scikit-learn==0.23.1',
