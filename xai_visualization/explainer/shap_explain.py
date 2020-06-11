0cddcd682f91eef28765a916a3b705d8b490f136
Roman <roman.buchholz@t-online.de>
2020-06-11 18:07:23 +0200

    finished shap explainer

--- xai_visualization/explainer/shap_explain.py
+++ xai_visualization/explainer/shap_explain.py
@@ -1,15 +1,16 @@
 import shap
-import datetime
 import numpy as np
 import tensorflow as tf
+from xai_visualization.util.load_data import feature_names
 
 def explain(model, samples):
 
-    samples = samples[:10]
+    # samples = samples[:10]
 
     background_dataset = samples[:1000]
     explainer = shap.DeepExplainer(model, background_dataset)
     probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+
 
     data = []
 
@@ -17,10 +18,14 @@ def explain(model, samples):
         # output format: [samples, classes, features(18)]
         per_class_explanations = np.squeeze(explainer.shap_values(np.array([sample])))
         data.append({
-            'output': np.argmax(probability_model.predict(np.array([sample]))),
-            'explanations': per_class_explanations
+            'input': sample.tolist(),
+            'output': probability_model.predict(np.array([sample])).tolist(),
+            'explanations': per_class_explanations.tolist()
         })
 
-    json = { 'data': data }
+    json = {'sampleRate': 25,
+            'labels': feature_names,
+            'data': data
+            }
 
     return json
