022f1ee6c2b000ca720c45f08a3f28a09076d5c8
Maximilian Ammann <max@maxammann.org>
2020-06-11 18:09:12 +0200

    Remove simplification

--- xai_visualization/explainer/lime_explain.py
+++ xai_visualization/explainer/lime_explain.py
@@ -4,8 +4,6 @@ import tensorflow as tf
 from xai_visualization.util.load_data import feature_names
 
 def explain(model, samples):
-    samples = samples[:10]
-
     background_dataset = samples[:1000]
     explainer = LimeTabularExplainer(background_dataset, feature_names=feature_names, discretize_continuous=False)
     probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
