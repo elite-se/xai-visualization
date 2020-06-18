00ecf4288d4bddd73a24a6b8117c1ca26cae9283
Michael Markl <marklmichael98@gmail.com>
2020-06-18 18:36:27 +0200

    #23: Smooth data

---
+++ frontend/src/generateMockData.ts
@@ -0,0 +1,74 @@
+import {reduce} from "lodash";
+import { DataPointType } from "./loadEngagementData";
+
+const createInterpolator = (label: string, numberPoints: number) => {
+    // quadratic bezier based on label
+    let seed = reduce(label.split(''), (acc: number, char: string) => acc + char.charCodeAt(0), 0)
+    const random = () => {
+        const x = Math.sin(seed++) * 10000;
+        return x - Math.floor(x);
+    }
+    const y = Array.from(Array(numberPoints).keys()).map(random)
+
+    return (t: number) => {
+        const firstPoint = Math.floor(t * numberPoints / 2) * 2
+        const y0 = y[firstPoint]
+        const y1 = y[firstPoint + 1]
+        const y2 = y[firstPoint + 2]
+
+        const t1 = (t - firstPoint / numberPoints) * numberPoints / 2
+        return (y0 - 2 * y1 + y2) * t1 * t1 + (-2 * y0 + 2 * y1) * t1 + y0
+    }
+}
+
+const generateMockData = () => {
+    const sampleRate = 25
+    const videoDuration = 1140.52 // Sample video
+    const labels = [
+        '0 Valence from Face (emax)',
+        '1 face horizontal movement (emax)',
+        '2 face vertical movement (emax)',
+        '3 armscrossed',
+        '4 headtouch',
+        '5 distance left hand left hip',
+        '6 distance right hand right hip',
+        '7 left elbow y rotation',
+        '8 right elbow y rotation',
+        '9 hand in front of left hip',
+        '10 hand in front of right hip',
+        '11 left elbow x rotation',
+        '12 right elbow x rotation',
+        '13 standard deviation head x position',
+        '14 standard deviation head x rotation',
+        '15 voice activity',
+        '16 Skeleton overall activation',
+        '17 Skeleton energy global max'
+    ]
+
+    const numberSamples = Math.ceil(videoDuration * sampleRate)
+    const numberPoints = Math.max(Math.floor(videoDuration / 5) * 2, 2) + 1 // Ca. alle 20 Sekunden neue Bezier Kurve
+
+    const data: DataPointType[] = []
+
+    const inputInterpolators = labels.map(label => createInterpolator('i' + label, numberPoints))
+    const outputInterpolators = ['0o', '1o', '2o', '3o'].map(label => createInterpolator(label, numberPoints))
+    const explanationInterpolators = ['0e', '1e', '2e', '3e'].map(
+        group => labels.map(label => createInterpolator(group + label, numberPoints))
+    )
+
+    for (let i = 0; i < numberSamples; i++) {
+        const t = i / numberSamples
+        const input = inputInterpolators.map(interpolate => interpolate(t))
+        const output = outputInterpolators.map(interpolate => interpolate(t))
+        const outputSum = output.reduce((acc, value) => acc + value, 0)
+        const normalizedOutput = output.map(value => value / outputSum)
+        const explanations = explanationInterpolators.map(
+            interpolators => interpolators.map(interpolate => interpolate(t))
+        )
+
+        data.push({input, output: normalizedOutput, explanations})
+    }
+    return {sampleRate, labels, data}
+}
+
+export default generateMockData
--- frontend/src/loadEngagementData.ts
+++ frontend/src/loadEngagementData.ts
@@ -1,27 +1,6 @@
-import {reduce} from "lodash"
+import generateMockData from "./generateMockData";
 
-
-const createInterpolator = (label: string, numberPoints: number) => {
-    // quadratic bezier based on label
-    let seed = reduce(label.split(''), (acc: number, char: string) => acc + char.charCodeAt(0), 0)
-    const random = () => {
-        const x = Math.sin(seed++) * 10000;
-        return x - Math.floor(x);
-    }
-    const y = Array.from(Array(numberPoints).keys()).map(random)
-
-    return (t: number) => {
-        const firstPoint = Math.floor(t * numberPoints / 2) * 2
-        const y0 = y[firstPoint]
-        const y1 = y[firstPoint + 1]
-        const y2 = y[firstPoint + 2]
-
-        const t1 = (t - firstPoint / numberPoints) * numberPoints / 2
-        return (y0 - 2 * y1 + y2) * t1 * t1 + (-2 * y0 + 2 * y1) * t1 + y0
-    }
-}
-
-type DataPointType = { input: number[], output: number[], explanations: number[][] }
+export type DataPointType = { input: number[], output: number[], explanations: number[][] }
 
 export type DataContainerType = {
     sampleRate: number,
@@ -29,60 +8,55 @@ export type DataContainerType = {
     data: DataPointType[]
 }
 
+const AVG_WINDOW_SECONDS = 10 // Moving Average Window Size in seconds
+const smoothData = (data: DataPointType[], windowSize: number): DataPointType[] => {
+    const smoothedData: DataPointType[] = Array(data.length)
+    for (let i = 0; i < data.length; i++) {
+        if (i === 0) {
+            smoothedData[i] = data[i]
+        } else if (i < windowSize) { // Adding the i+1-st item to the window
+            // Formula: (smoothedData[i - 1] * i + data[i]) / (i + 1)
+            const smoothedExplanations = data[i].explanations.map((row, j) => row.map((val, k) =>
+                (smoothedData[i-1].explanations[j][k] * i + val) / (i+1)
+                ))
+            const smoothedInput = data[i].input.map((val, j) =>
+                (smoothedData[i-1].input[j] * i + val) / (i+1)
+            )
+            const smoothedOutput = data[i].output.map((val, j) =>
+                (smoothedData[i-1].output[j] * i + val) / (i+1)
+            )
+
+            smoothedData[i] = { explanations: smoothedExplanations, input: smoothedInput, output: smoothedOutput }
+        } else {
+            // Formula: smoothedData[i - 1] + (data[i] - data[i-windowSize]) / windowSize
+            const smoothedExplanations = data[i].explanations.map((row, j) => row.map((val, k) =>
+                smoothedData[i-1].explanations[j][k] + (val - data[i-windowSize].explanations[j][k]) / windowSize
+            ))
+            const smoothedInput = data[i].input.map((val, j) =>
+                smoothedData[i-1].input[j] + (val - data[i-windowSize].input[j]) / windowSize
+            )
+            const smoothedOutput = data[i].output.map((val, j) =>
+                smoothedData[i-1].output[j] + (val - data[i-windowSize].output[j]) / windowSize
+            )
+
+            smoothedData[i] = { explanations: smoothedExplanations, input: smoothedInput, output: smoothedOutput }
+        }
+    }
+    return smoothedData
+}
+
 const loadEngagementData = async (username: string, password: string) => {
     try {
         const credentials = username + ':' + password
         const response = await fetch('https://xn--ls8h.maxammann.org/001_2016-03-17_Paris/expert.video.mp4.json',
-            { headers: { 'Authorization': 'Basic ' + window.btoa(credentials || '')}})
-        return response.json()
+            {headers: {'Authorization': 'Basic ' + window.btoa(credentials || '')}})
+        const dataContainer: DataContainerType = await response.json()
+        const windowSize = AVG_WINDOW_SECONDS * dataContainer.sampleRate
+        dataContainer.data = smoothData(dataContainer.data, windowSize)
+        return dataContainer
     } catch (e) {
-        const sampleRate = 25
-        const videoDuration = 1140.52 // Sample video
-        const labels = [
-            '0 Valence from Face (emax)',
-            '1 face horizontal movement (emax)',
-            '2 face vertical movement (emax)',
-            '3 armscrossed',
-            '4 headtouch',
-            '5 distance left hand left hip',
-            '6 distance right hand right hip',
-            '7 left elbow y rotation',
-            '8 right elbow y rotation',
-            '9 hand in front of left hip',
-            '10 hand in front of right hip',
-            '11 left elbow x rotation',
-            '12 right elbow x rotation',
-            '13 standard deviation head x position',
-            '14 standard deviation head x rotation',
-            '15 voice activity',
-            '16 Skeleton overall activation',
-            '17 Skeleton energy global max'
-        ]
-
-        const numberSamples = Math.ceil(videoDuration * sampleRate)
-        const numberPoints = Math.max(Math.floor(videoDuration / 5) * 2, 2) + 1 // Ca. alle 20 Sekunden neue Bezier Kurve
-
-        const data: DataPointType[] = []
-
-        const inputInterpolators = labels.map(label => createInterpolator('i' + label, numberPoints))
-        const outputInterpolators = ['0o', '1o', '2o', '3o'].map(label => createInterpolator(label, numberPoints))
-        const explanationInterpolators = ['0e', '1e', '2e', '3e'].map(
-            group => labels.map(label => createInterpolator(group + label, numberPoints))
-        )
-
-        for (let i = 0; i < numberSamples; i++) {
-            const t = i / numberSamples
-            const input = inputInterpolators.map(interpolate => interpolate(t))
-            const output = outputInterpolators.map(interpolate => interpolate(t))
-            const outputSum = output.reduce((acc, value) => acc + value, 0)
-            const normalizedOutput = output.map(value => value / outputSum)
-            const explanations = explanationInterpolators.map(
-                interpolators => interpolators.map(interpolate => interpolate(t))
-            )
-
-            data.push({input, output: normalizedOutput, explanations})
-        }
-        return {sampleRate, labels, data}
+        console.error('Error loading or smoothing data. Using mocks')
+        return generateMockData()
     }
 }
 
