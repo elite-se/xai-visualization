743655fb5fc272b19869d77627f6793b89bc0954
Maximilian Ammann <max@maxammann.org>
2020-06-19 14:08:37 +0200

    Add output parameter

--- xai_visualization/explainer/lime_explain.py
+++ xai_visualization/explainer/lime_explain.py
@@ -3,6 +3,24 @@ import numpy as np
 import tensorflow as tf
 from xai_visualization.util.load_data import feature_names
 from tqdm import tqdm
+import multiprocessing
+
+
+def process_sample(explainer, probability_model, sample):
+    explanation = explainer.explain_instance(
+        sample, probability_model.predict, top_labels=4, num_features=18)
+
+    explanations = np.array([explanation.local_exp[i]
+                             for i in map(int, explanation.local_exp.keys())])
+    indices = np.argsort(explanations[:, :, 0].astype(int), axis=1)
+    per_class_explanations = np.take_along_axis(
+        explanations[:, :, 1], indices, axis=1)
+
+    return {
+        'input': sample.tolist(),
+        'output': explanation.predict_proba.tolist(),
+        'explanations': per_class_explanations.tolist()
+    }
 
 
 def explain(model, samples):
@@ -15,20 +33,7 @@ def explain(model, samples):
     data = []
 
     for sample in tqdm(samples):
-        explanation = explainer.explain_instance(
-            sample, probability_model.predict, top_labels=4, num_features=18)
-
-        explanations = np.array([explanation.local_exp[i]
-                                 for i in map(int, explanation.local_exp.keys())])
-        indices = np.argsort(explanations[:, :, 0].astype(int), axis=1)
-        per_class_explanations = np.take_along_axis(
-            explanations[:, :, 1], indices, axis=1)
-
-        data.append({
-            'input': sample.tolist(),
-            'output': explanation.predict_proba.tolist(),
-            'explanations': per_class_explanations.tolist()
-        })
+        data.append(process_sample(explainer, probability_model, sample))
 
     json = {
         'sampleRate': 25,
