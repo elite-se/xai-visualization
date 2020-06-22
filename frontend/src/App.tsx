b789a37c695184e61ec38daa1a783c9e9f3e5fbe
FabianWildgrube <fabian.wildgrube@gmx.de>
2020-06-22 12:02:34 +0200

    #27 added second participant (expert) to GUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {SyntheticEvent} from "react";
+import React, { SyntheticEvent } from "react";
 import Participant from "./Participant";
 
 import {
@@ -9,11 +9,11 @@ import {
     Navbar,
     NavbarDivider,
     NavbarGroup,
-    NavbarHeading
+    NavbarHeading,
 } from "@blueprintjs/core";
-import {IconNames} from "@blueprintjs/icons";
+import { IconNames } from "@blueprintjs/icons";
 import styled from "styled-components";
-import loadEngagementData, {DataContainerType} from "./loadEngagementData";
+import loadEngagementData, { DataContainerType } from "./loadEngagementData";
 
 const Container = styled.div`
     min-height: 100vh;
@@ -22,53 +22,92 @@ const Container = styled.div`
 `;
 
 const Main = styled.main`
-    padding: 40px;
+    padding: 30px 40px;
 `;
 
 const H3 = styled.div`
     margin-left: 6px;
 `;
 
-type StateType = { username: string, password: string, dataContainer: DataContainerType | null}
+const HorizontalSpacer = styled.div`
+    height: 20px;
+`;
+
+type ParticipantData = { name: string; videoURL: string; dataURL: string; dataContainer: DataContainerType | null };
+
+type StateType = { username: string; password: string; participantsData: ParticipantData[] };
 
 class App extends React.Component<{}, StateType> {
-    state: StateType = { username: '', password: '', dataContainer: null}
+    state: StateType = {
+        username: "",
+        password: "",
+        participantsData: [
+            {
+                name: "Ian Jackson",
+                videoURL: "https://xn--ls8h.maxammann.org/001_2016-03-17_Paris/expert.video.mp4",
+                dataURL: "https://xn--ls8h.maxammann.org/001_2016-03-17_Paris/lime-expert.video.mp4.json",
+                dataContainer: null,
+            },
+            {
+                name: "Allister McCrane",
+                videoURL: "https://xn--ls8h.maxammann.org/001_2016-03-17_Paris/novice.video.mp4",
+                dataURL: "https://xn--ls8h.maxammann.org/001_2016-03-17_Paris/lime-novice.video.mp4.json",
+                dataContainer: null,
+            },
+        ],
+    };
 
     onUsernameChange = (event: SyntheticEvent<HTMLInputElement>) => {
-        this.setState({ username: event.currentTarget.value })
-    }
+        this.setState({ username: event.currentTarget.value });
+    };
     onPasswordChange = (event: SyntheticEvent<HTMLInputElement>) => {
-        this.setState({ password: event.currentTarget.value })
-    }
+        this.setState({ password: event.currentTarget.value });
+    };
     loadData = async () => {
-        const dataContainer = await loadEngagementData(this.state.username, this.state.password)
-        this.setState({dataContainer})
-    }
-
-    renderParticipant = () => {
-        const { dataContainer } = this.state
-        if (dataContainer !== null) {
-            return <Participant
-                dataContainer={dataContainer}
-                name="John Doe"
-                videoURL="https://xn--ls8h.maxammann.org/001_2016-03-17_Paris/novice.video.mp4"
-            />
+        const { participantsData } = this.state;
+        for (let pData of participantsData) {
+            pData.dataContainer = await loadEngagementData(this.state.username, this.state.password, pData.dataURL);
         }
-        return null
-    }
+        this.setState({ participantsData });
+    };
+
+    renderParticipants = () => {
+        let participants = [];
+        const { participantsData } = this.state;
+        let i = 0;
+        for (let pData of participantsData) {
+            if (pData.dataContainer !== null) {
+                participants.push(
+                    <Participant
+                        key={"p" + i}
+                        dataContainer={pData.dataContainer}
+                        name={pData.name}
+                        videoURL={pData.videoURL}
+                    />
+                );
+                if (i < participantsData.length - 1) {
+                    participants.push(<HorizontalSpacer key={"s" + i} />);
+                }
+            }
+            i++;
+        }
+        return participants;
+    };
 
     render() {
         return (
             <Container>
                 <Navbar>
                     <NavbarGroup>
-                        <Icon icon={IconNames.FULL_STACKED_CHART} iconSize={Icon.SIZE_LARGE}/>
+                        <Icon icon={IconNames.FULL_STACKED_CHART} iconSize={Icon.SIZE_LARGE} />
                         <NavbarHeading>
                             <H3>XAI-Visualisations</H3>
                         </NavbarHeading>
-                        <NavbarDivider/>
+                        <NavbarDivider />
                         <ButtonGroup>
-                            <Button icon='document-open' onClick={() => alert('tbd')}>Open...</Button>
+                            <Button icon="document-open" onClick={() => alert("tbd")}>
+                                Open...
+                            </Button>
                         </ButtonGroup>
                         <NavbarDivider />
                         <InputGroup placeholder="Username" onChange={this.onUsernameChange} />
@@ -77,9 +116,7 @@ class App extends React.Component<{}, StateType> {
                     </NavbarGroup>
                 </Navbar>
 
-                <Main>
-                    {this.renderParticipant()}
-                </Main>
+                <Main>{this.renderParticipants()}</Main>
             </Container>
         );
     }
